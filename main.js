c81a06ac5d8afab29edd93e90321307e425d0f42
Titus Woo <titus.woo@me.com>
2016-04-19 17:38:47 -0400

    User can select a video, and can play it. Each frame is grayscaled in real-time and displayed on the right in a canvas element. Wrote a little pipeline lib to abstract away operations with the canvas contexts and video events. Proof of concept to get it all working nicely. TODO: next steps are to get basic seam carving implemented.

---
+++ main.js
@@ -0,0 +1,40 @@
+// http://html5doctor.com/video-canvas-magic/
+
+'use strict';
+
+$(function() {
+  var originalVideo = document.querySelector('#video-player');
+  var preview = document.querySelector('#video-preview');
+  var videoPipeline = pipeline(originalVideo);
+
+  $('#fileUpload').on('change', function() {
+    var file = $(this)[0].files[0];
+    var fileURL = URL.createObjectURL(file);
+    var video = videoPipeline(fileURL);
+
+    video.on('loaded', function(video) {
+      preview.width = video.clientWidth;
+      preview.height = video.clientHeight;
+    });
+
+    video.imageStream(function(imageData) {
+      var previewCtx = preview.getContext('2d');
+      imageData = modifyFrame(imageData);
+      previewCtx.putImageData(imageData, 0, 0);
+    });
+  });
+});
+
+function modifyFrame(imageData) {
+  var data = imageData.data;
+  for(var i = 0; i < data.length; i+=4) {
+    var r = data[i];
+    var g = data[i+1];
+    var b = data[i+2];
+    var brightness = (3*r+4*g+b)>>>3;
+    data[i] = brightness;
+    data[i+1] = brightness;
+    data[i+2] = brightness;
+    }
+  return imageData;
+}
---
+++ pipeline.js
@@ -0,0 +1,84 @@
+var pipeline = (function() {
+  'use strict';
+
+  return pipeline;
+
+  function pipeline(video) {
+    var events = eventsManager();
+    var canvas = document.createElement('canvas');
+    var context = canvas.getContext('2d');
+
+    var width, height;
+
+    var redrawInterval;
+
+    video.addEventListener('playing', function() {
+      redrawInterval = setInterval(function() {
+        var imageData = _getImageDataFromVideo();
+        events.trigger('imageStream', imageData);
+      }, 0);
+    });
+
+    video.addEventListener('seeked', function() {
+      events.trigger('imageStream',
+        _getImageDataFromVideo()
+      );
+    });
+
+    video.addEventListener('pause', function() {
+      clearInterval(redrawInterval);
+    });
+
+    video.addEventListener('loadeddata', function() {
+      width = video.clientWidth;
+      height = video.clientHeight;
+
+      canvas.width = width;
+      canvas.height = height;
+
+      events.trigger('loaded', video);
+      events.trigger('imageStream',
+        _getImageDataFromVideo()
+      );
+    });
+
+    function _getImageDataFromVideo() {
+      context.drawImage(video, 0, 0, width, height);
+      return context.getImageData(0, 0, width, height);
+    }
+
+    return function(fileURL) {
+      video.src = fileURL;
+      var eventListeners = [];
+
+      return {
+        on: function(eventName, cb) {
+          events.register(eventName, cb);
+        },
+        imageStream: function(cb) {
+          events.register('imageStream', cb);
+        }
+      };
+    };
+  }
+
+  function eventsManager() {
+    var registeredEvents = [];
+
+    return {
+      trigger: function(eventName, args) {
+        registeredEvents.filter(function(event) {
+          return event.name === eventName;
+        }).forEach(function(event) {
+          event.cb.call(this, args);
+        });
+      },
+      register: function(eventName, cb) {
+        registeredEvents.push({
+          name: eventName,
+          cb: cb
+        });
+      }
+    };
+  }
+})();
