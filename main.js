7adc532b841346158805ba49739c001b564b869c
Titus Woo <titus.woo@me.com>
2016-04-25 14:59:35 -0400

    removed jquery and socket.io dependencies.

--- main.js
+++ main.js
@@ -1,15 +1,16 @@
-(function($, tooling) {
+(function(tooling) {
   'use strict';
-  $(function() {
+  window.onload = function() {
     var originalVideo = document.querySelector('#video-player');
     var videoContainers = document.querySelector('#video-containers');
     var preview = document.querySelector('#video-preview');
+    var fileUpload = document.querySelector('#fileUpload');
     var videoPipeline = pipeline(originalVideo);
-
+    
     videoContainers.style.visibility = 'hidden';
 
-    $('#fileUpload').on('change', function() {
-      var file = $(this)[0].files[0];
+    fileUpload.addEventListener('change', function(e) {
+      var file = this.files[0];
       var fileURL = URL.createObjectURL(file);
       var video = videoPipeline(fileURL);
 
@@ -25,11 +26,10 @@
         previewCtx.putImageData(imageData, 0, 0);
       });
     });
-  });
+  };
 
   function modifyFrame(imageData) {
     var grayscale = tooling.use('grayscale');
-    console.log(grayscale);
     return grayscale(imageData);
   }
-})(jQuery, tooling);
+})(tooling);
