b3f9db2c951f8296dfcad297d2ff6bfc877fd8da
amolpatel <apatel374@gatech.edu>
2016-04-29 16:56:57 -0400

    get frames and recompile

--- server.js
+++ server.js
@@ -2,42 +2,47 @@ var express = require('express');
 var app = express();
 var ffmpeg = require('fluent-ffmpeg');
 
-// var mergedVideo = ffmpeg()
-// ;
-// var videoNames = ['./flag.mp4', './flag.mp4'];
+var splitVid = new ffmpeg();
 
-// videoNames.forEach(function(videoName){
-//     mergedVideo = mergedVideo.addInput(videoName);
-// });
-
-// mergedVideo.mergeToFile('./mergedVideo.mp4', './tmp/')
-// .on('error', function(err) {
-//     console.log('Error ' + err.message);
-// })
-// .on('end', function() {
-//     console.log('Finished!');
-// });
-
-//-------------------
-
-// var thumbler = require('video-thumb');
-// thumbler.extract('flag.mp4', 'snapshot.png', '00:00:02', '853x480', function(){
-
-//     console.log('snapshot saved to snapshot.png (200x125) with a frame at 00:00:22');
-
-// });
-
-// var progress = new ffmpeg('./flag.mp4')
-//   .on('progress', function(progress) {
-//   	console.log(progress.currentFps)
-//   });
+splitVid.addInput('./flag.mp4')
+.on('start', function(ffmpegCommand) {
+    /// log something maybe
+})
+.on('progress', function(data) {
+    /// do stuff with progress data if you want
+})
+.on('end', function() {
+    /// encoding is complete, so callback or move on at this point
+})
+.on('error', function(error) {
+    /// error handling
+})
+.outputOptions(['-vf fps=30 '])
+.output('output/out%d.png')
+.run();
 
 
-var proc = new ffmpeg('./flag.mp4').native()
-  .takeScreenshots({
-      count: 240
-    }, './output', function(err) {
-    console.log('screenshots were saved')
-  });
 
 
+
+var ffmpeg = require('fluent-ffmpeg');
+
+var proc = new ffmpeg();
+
+proc.addInput('output/out%d.png')
+.on('start', function(ffmpegCommand) {
+    /// log something maybe
+})
+.on('progress', function(data) {
+    /// do stuff with progress data if you want
+})
+.on('end', function() {
+    /// encoding is complete, so callback or move on at this point
+})
+.on('error', function(error) {
+    /// error handling
+})
+.addInputOption('-framerate 30')
+.outputOptions(['-c:v libx264','-pix_fmt yuv420p'])
+.output('out.mp4')
+.run();
