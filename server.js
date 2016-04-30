0306909daf7ca1ed42f3e8e135b1c54c38869208
amolpatel <apatel374@gatech.edu>
2016-04-30 13:58:54 -0400

    added server.js

---
+++ server.js
@@ -0,0 +1,63 @@
+var express = require('express'),
+    multer  = require('multer');
+var app = express();
+var ffmpeg = require('fluent-ffmpeg');
+
+var multer = require('multer');
+var upload = multer({ dest: './uploads' });
+
+app.post('/', function (req, res) {
+    console.log(req.body); //contains the variables
+    console.log(req.files); //contains the file references
+    res.send('Thank you for uploading!');
+});
+
+app.listen(8000);
+
+
+
+
+// var splitVid = new ffmpeg();
+//
+// splitVid.addInput('./flag.mp4')
+// .on('start', function(ffmpegCommand) {
+//     /// log something maybe
+// })
+// .on('progress', function(data) {
+//     /// do stuff with progress data if you want
+// })
+// .on('end', function() {
+//     /// encoding is complete, so callback or move on at this point
+// })
+// .on('error', function(error) {
+//     /// error handling
+// })
+// .outputOptions(['-vf fps=30 '])
+// .output('output/out%d.png')
+// .run();
+//
+//
+//
+//
+//
+// var ffmpeg = require('fluent-ffmpeg');
+//
+// var proc = new ffmpeg();
+//
+// proc.addInput('output/out%d.png')
+// .on('start', function(ffmpegCommand) {
+//     /// log something maybe
+// })
+// .on('progress', function(data) {
+//     /// do stuff with progress data if you want
+// })
+// .on('end', function() {
+//     /// encoding is complete, so callback or move on at this point
+// })
+// .on('error', function(error) {
+//     /// error handling
+// })
+// .addInputOption('-framerate 30')
+// .outputOptions(['-c:v libx264','-pix_fmt yuv420p'])
+// .output('out.mp4')
+// .run();
