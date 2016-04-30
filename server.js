b1d737c2ffde209ea6e444846d3cc11d4d17fa19
Pranil Vora <pvora7@gatech.edu>
2016-04-30 15:09:35 -0400

    socket server stuff

--- server.js
+++ server.js
@@ -1,21 +1,39 @@
-var express = require('express'),
-    multer  = require('multer');
-var app = express();
-var ffmpeg = require('fluent-ffmpeg');
+// var express = require('express'),
+//     multer  = require('multer');
+// var app = express();
+// var ffmpeg = require('fluent-ffmpeg');
+//
+// var multer = require('multer');
+// var upload = multer({ dest: './uploads' });
+//
+// app.post('/', function (req, res) {
+//     console.log(req.body); //contains the variables
+//     console.log(req.files); //contains the file references
+//     res.send('Thank you for uploading!');
+// });
+//
+// app.listen(8000);
 
-var multer = require('multer');
-var upload = multer({ dest: './uploads' });
 
-app.post('/', function (req, res) {
-    console.log(req.body); //contains the variables
-    console.log(req.files); //contains the file references
-    res.send('Thank you for uploading!');
-});
+var app = require('http').createServer(handler)
+  , io = require('socket.io').listen(app)
+  , fs = require('fs')
+  , exec = require('child_process').exec
+  , util = require('util')
 
-app.listen(8000);
-
-
-
+app.listen(8080);
+//
+function handler (req, res) {
+  fs.readFile(__dirname + '/index.html',
+  function (err, data) {
+    if (err) {
+      res.writeHead(500);
+      return res.end('Error loading index.html');
+    }
+    res.writeHead(200);
+    res.end(data);
+  });
+}
 
 // var splitVid = new ffmpeg();
 //
